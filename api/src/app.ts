ffbe04329302b83b784f683e8ef508e07a5270ce
Harshit Pareek <hp1014@nyu.edu>
2020-03-31 14:25:42 -0700

    feat(adding the nginx but not working)

--- api/src/app.ts
+++ api/src/app.ts
@@ -13,7 +13,7 @@ import redis from 'redis';
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
